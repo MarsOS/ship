a41bab64434a8376bcc33b981e36e5839aa28f10
Marius Messerschmidt <marius.messerschmidt@googlemail.com>
2017-08-09 17:48:33 +0200

    ship will now extract the files and create a list of those files
    Signed-off-by: Marius Messerschmidt <marius.messerschmidt@googlemail.com>

--- Makefile
+++ Makefile
@@ -7,8 +7,10 @@ COMMIT=\"$(shell git rev-parse --verify HEAD)\"
 ARCH=\"$(shell uname -m)\"
 DBPATH=\"./ship.sqlite3\"
 DBLOCKPATH=\"./ship.sqlite3.lock\"
+INSTALLPATH=\"./testroot\"
+
 build:
-	$(CC) $(CPPFLAGS) -D DBLOCKPATH=$(DBLOCKPATH) -D DBPATH=$(DBPATH) -D VERSION=$(VERSION) -D ARCH=$(ARCH) -D COMMIT=$(COMMIT) $(SOURCES) $(LDFLAGS) -o ship
+	$(CC) $(CPPFLAGS) -D INSTALLPATH=$(INSTALLPATH) -D DBLOCKPATH=$(DBLOCKPATH) -D DBPATH=$(DBPATH) -D VERSION=$(VERSION) -D ARCH=$(ARCH) -D COMMIT=$(COMMIT) $(SOURCES) $(LDFLAGS) -o ship
 
 version:
 	@echo $(VERSION) | sed s/\"//g
--- src/installer.cpp
+++ src/installer.cpp
@@ -7,6 +7,8 @@ using namespace std;
 
 #include <unistd.h>
 #include <sys/types.h>
+#include <archive.h>
+
 
 PackageInstaller::PackageInstaller(string path)
 {
@@ -38,6 +40,81 @@ void PackageInstaller::install()
   cout << "=> Locked Database" << endl;
 
 
+  cout << "=> Extracting Files" << endl;
+
+  struct archive *pkg;
+  struct archive *out;
+  struct archive_entry *entry;
+
+  pkg = archive_read_new();
+  archive_read_support_format_all(pkg);
+  archive_read_support_filter_gzip(pkg);
+
+  int r = archive_read_open_filename(pkg, this->pi->getPath().c_str(), 10240);
+  if(r)
+  {
+    cerr << "Error: Could not open package file to extract" << endl;
+    exit(1);
+  }
+
+  out = archive_write_disk_new();
+  archive_write_disk_set_options(out, ARCHIVE_EXTRACT_TIME | ARCHIVE_EXTRACT_PERM | ARCHIVE_EXTRACT_ACL | ARCHIVE_EXTRACT_FFLAGS);
+  archive_write_disk_set_standard_lookup(out);
+
+  string files = "";
+
+  while(1)
+  {
+    r = archive_read_next_header(pkg, &entry);
+    if(r == ARCHIVE_EOF)
+      break;
+    else if(r < ARCHIVE_OK)
+      cerr << "Warning: " << archive_error_string(pkg);
+
+    string path = archive_entry_pathname(entry);
+    if(path.compare(0, 4, "root") != 0)
+    {
+      archive_read_data_skip(pkg);
+      continue;
+    }
+
+    path.replace(0, 4, INSTALLPATH);
+    files += path + ";";
+    archive_entry_set_pathname(entry, path.c_str());
+
+    r = archive_write_header(out, entry);
+    if(r < ARCHIVE_OK)
+    {
+      cerr << "Warning: " << archive_error_string(out) << endl;
+    }
+    else if (archive_entry_size(entry) > 0)
+    {
+      const void *buff;
+      size_t size;
+      long offset;
+      while(1)
+      {
+        r = archive_read_data_block(pkg, &buff, &size, &offset);
+        if(r == ARCHIVE_EOF)
+        {
+          break;
+        }
+        if(r < ARCHIVE_OK)
+        {
+          cerr << "Error: " << archive_error_string(pkg);
+          break;
+        }
+
+        r = archive_write_data_block(out, buff, size, offset);
+        if(r < ARCHIVE_OK)
+        {
+          cerr << "Error: " << archive_error_string(out);
+          break;
+        }
+      }
+    }
+  }
+
   cout << "=> Updating Package Database" << endl;
 
   DBWrapper db = DBWrapper();
@@ -48,7 +125,7 @@ void PackageInstaller::install()
       this->pi->getName(),
       this->pi->getVersion(),
       this->pi->getBlueprint().dump(),
-      "",
+      files,
       this->pi->getSize(),
       getuid()
     );
@@ -60,7 +137,7 @@ void PackageInstaller::install()
       this->pi->getVersion(),
       this->pi->getBlueprint().dump(),
       0,
-      "",
+      files,
       this->pi->getSize(),
       getuid()
     );
